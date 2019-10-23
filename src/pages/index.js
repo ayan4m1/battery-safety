5b14e2a0b40f609b599debd78528cb3b067fe338
ayan4m1 <andrew@bulletlogic.com>
2019-10-23 08:27:11 -0400

    overhaul gatsby starter - add sass, eslint, prettier, stylelint, husky, lint-staged

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react';
+import { Link } from 'gatsby';
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import '../index.scss';
+
+import Layout from '../components/layout';
+import Image from '../components/image';
+import SEO from '../components/seo';
 
 const IndexPage = () => (
   <Layout>
@@ -16,6 +18,6 @@ const IndexPage = () => (
     </div>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;
