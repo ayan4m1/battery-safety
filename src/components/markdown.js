2fb85dad0fed6a90e67f900a67923e7171f9db9b
ayan4m1 <andrew@bulletlogic.com>
2019-10-25 03:12:21 -0400

    add markdown component and update index page to be a list of posts

---
+++ src/components/markdown.js
@@ -0,0 +1,48 @@
+import { graphql } from 'gatsby';
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+
+import SEO from '~components/seo';
+import Layout from '~components/layout';
+
+export default class Markdown extends Component {
+  static propTypes = {
+    data: PropTypes.object.isRequired
+  };
+
+  render() {
+    const { data } = this.props;
+    const {
+      frontmatter: { title },
+      html
+    } = data.markdownRemark;
+
+    return (
+      <Layout>
+        <SEO title={title} />
+        <Container>
+          <Row>
+            <Col md="12">
+              <h1>{title}</h1>
+            </Col>
+            <Col md="12" dangerouslySetInnerHTML={{ __html: html }} />
+          </Row>
+        </Container>
+      </Layout>
+    );
+  }
+}
+
+export const query = graphql`
+  query($path: String!) {
+    markdownRemark(frontmatter: { path: { eq: $path } }) {
+      html
+      frontmatter {
+        date(formatString: "MMMM DD, YYYY")
+        path
+        title
+      }
+    }
+  }
+`;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,48 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { graphql, Link } from 'gatsby';
 
 import '../index.scss';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Recent tests</h1>
+    {data.allMarkdownRemark.edges.map(({ node }) => (
+      <div key={node.frontmatter.path}>
+        <Link to={node.frontmatter.path}>
+          <h2>{node.frontmatter.title}</h2>
+        </Link>
+        <p className="text-muted">published on {node.frontmatter.date}</p>
+      </div>
+    ))}
   </Layout>
 );
+
+IndexPage.propTypes = {
+  data: PropTypes.object.isRequired
+};
+
+export const query = graphql`
+  query RecentPostsQuery {
+    allMarkdownRemark(
+      sort: { order: DESC, fields: [frontmatter___date] }
+      limit: 1000
+    ) {
+      edges {
+        node {
+          frontmatter {
+            date
+            path
+            title
+          }
+        }
+      }
+    }
+  }
+`;
 
 export default IndexPage;
