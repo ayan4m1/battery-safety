bf3c54b66b1079779b5f8e823ca96afd5c7760ca
ayan4m1 <andrew@bulletlogic.com>
2019-10-25 03:12:00 -0400

    add code to create pages for test entries and set up webpack aliases

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,45 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const { resolve } = require('path');
 
-// You can delete this file if you're not using it
+exports.createPages = async ({ actions, graphql, reporter }) => {
+  const component = resolve('src/components/markdown.js');
+  const { createPage } = actions;
+  const result = await graphql(`
+    {
+      allMarkdownRemark(
+        sort: { order: DESC, fields: [frontmatter___date] }
+        limit: 1000
+      ) {
+        edges {
+          node {
+            frontmatter {
+              path
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  if (result.errors) {
+    reporter.panicOnBuild(`Error while running GraphQL query.`);
+    return;
+  }
+
+  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+    createPage({
+      path: node.frontmatter.path,
+      component
+    });
+  });
+};
+
+exports.onCreateWebpackConfig = ({ actions }) => {
+  actions.setWebpackConfig({
+    resolve: {
+      alias: {
+        '~components': resolve(__dirname, 'src/components'),
+        '~pages': resolve(__dirname, 'src/pages')
+      }
+    }
+  });
+};
