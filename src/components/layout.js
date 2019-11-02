e43d2ecf38d010ffe2ac2cb8ff33260aa0cbbf76
ayan4m1 <andrew@bulletlogic.com>
2019-11-02 15:10:22 -0400

    overhaul page container to be bootstrap-y

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,6 @@
-import React from 'react';
 import PropTypes from 'prop-types';
+import React, { Fragment } from 'react';
+import { Container } from 'react-bootstrap';
 import { useStaticQuery, graphql } from 'gatsby';
 
 import './layout.scss';
@@ -18,22 +19,15 @@ const Layout = ({ children }) => {
   `);
 
   return (
-    <>
+    <Fragment>
       <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0
-        }}
-      >
+      <Container>
         <main>{children}</main>
         <footer className="text-right mt-4">
-          © {new Date().getFullYear()} Vapor Wilco LLC
+          &copy; {new Date().getFullYear()} Vapor Wilco LLC
         </footer>
-      </div>
-    </>
+      </Container>
+    </Fragment>
   );
 };
 
