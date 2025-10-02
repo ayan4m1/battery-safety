d0192ded28096f90752c6aadf35648330da4ec97
ayan4m1 <andrew@bulletlogic.com>
2025-10-02 15:42:54 -0400

    chore: upgrade dependencies and remove react-helmet

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,35 +1,22 @@
-import PropTypes from 'prop-types';
 import { Fragment } from 'react';
+import PropTypes from 'prop-types';
 import { Container } from 'react-bootstrap';
-import { useStaticQuery, graphql } from 'gatsby';
 
 import './Layout.scss';
 
 import Header from './Header';
 
-const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `);
-
-  return (
-    <Fragment>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <Container>
-        <main>{children}</main>
-        <footer className="text-end mt-4">
-          &copy; {new Date().getFullYear()} Vapor Wilco LLC
-        </footer>
-      </Container>
-    </Fragment>
-  );
-};
+const Layout = ({ children }) => (
+  <Fragment>
+    <Header />
+    <Container>
+      <main>{children}</main>
+      <footer className="text-end mt-4">
+        &copy; {new Date().getFullYear()} Vapor Wilco LLC
+      </footer>
+    </Container>
+  </Fragment>
+);
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired
--- src/components/SEO.js
+++ src/components/SEO.js
@@ -6,10 +6,9 @@
  */
 
 import PropTypes from 'prop-types';
-import { Helmet } from 'react-helmet';
 import { useStaticQuery, graphql } from 'gatsby';
 
-function SEO({ description, lang, meta, title }) {
+function SEO({ description, title }) {
   const { site } = useStaticQuery(graphql`
     query {
       site {
@@ -25,60 +24,28 @@ function SEO({ description, lang, meta, title }) {
   const metaDescription = description || site.siteMetadata.description;
 
   return (
-    <Helmet
-      htmlAttributes={{
-        lang
-      }}
-      meta={[
-        {
-          name: `description`,
-          content: metaDescription
-        },
-        {
-          property: `og:title`,
-          content: title
-        },
-        {
-          property: `og:description`,
-          content: metaDescription
-        },
-        {
-          property: `og:type`,
-          content: `website`
-        },
-        {
-          name: `twitter:card`,
-          content: `summary`
-        },
-        {
-          name: `twitter:creator`,
-          content: site.siteMetadata.author
-        },
-        {
-          name: `twitter:title`,
-          content: title
-        },
-        {
-          name: `twitter:description`,
-          content: metaDescription
-        }
-      ].concat(meta)}
-      title={title}
-      titleTemplate={`%s | ${site.siteMetadata.title}`}
-    />
+    <head>
+      <meta content={metaDescription} name="description" />
+      <meta content={title} property="og:title" />
+      <meta content={metaDescription} property="og:description" />
+      <meta content="website" property="og:type" />
+      <meta content="summary" name="twitter:card" />
+      <meta content={site.siteMetadata.author} name="twitter:creator" />
+      <meta content={title} name="twitter:title" />
+      <meta content={metaDescription} name="twitter:description" />
+      <title>
+        {title} | {site.siteMetadata.title}
+      </title>
+    </head>
   );
 }
 
 SEO.defaultProps = {
-  lang: `en`,
-  meta: [],
   description: ``
 };
 
 SEO.propTypes = {
   description: PropTypes.string,
-  lang: PropTypes.string,
-  meta: PropTypes.arrayOf(PropTypes.object),
   title: PropTypes.string.isRequired
 };
 
